4d5750ee50b37d589d54839a795f79493bb8f949
fabian <fzamora_ro007@hotmail.com>
2020-06-18 15:42:13 -0500

    final sin dist con eviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA8YYWtniQNgrAyxqTGXArVNBg70l4KOi0",
+    authDomain: "controlmuebles.firebaseapp.com",
+    databaseURL: "https://controlmuebles.firebaseio.com",
+    projectId: "controlmuebles",
+    storageBucket: "controlmuebles.appspot.com",
+    messagingSenderId: "1003938286786",
+    appId: "1:1003938286786:web:6dba71a2123a341592bf5b",
+    measurementId: "G-GH0FTEHHEQ"
+
+  }
 };
